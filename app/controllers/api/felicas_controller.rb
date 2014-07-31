0120d21582c50bffe4b1abb0114bfaa1909701ca
asonas <hzw1258@gmail.com>
2014-07-31 17:32:12 +0900

    find or initialize

--- app/controllers/api/felicas_controller.rb
+++ app/controllers/api/felicas_controller.rb
@@ -1,7 +1,12 @@
 class Api::FelicasController < ApiController
 
   def create
-    felica = Felica.new(idm: params[:idm])
+    felica = Felica.where(idm: params[:idm]).first
+
+    if felica.nil?
+      felica = Felica.new(idm: params[:idm])
+    end
+
     if felica.save
       render text: 200
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+    @felica = Felica.find(params[:felica_id])
   end
 
   # GET /users/1/edit
