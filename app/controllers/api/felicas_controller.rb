553b25be80377f502d3fd7d5292175735cf8d790
asonas <hzw1258@gmail.com>
2014-07-31 14:23:42 +0900

    Add create action

--- app/controllers/api/felicas_controller.rb
+++ app/controllers/api/felicas_controller.rb
@@ -1,2 +1,12 @@
 class Api::FelicasController < ApiController
+
+  def create
+    felica = Felica.new(idm: params[:idm])
+    if felica.save
+      render text: 200
+    else
+      render text: 401
+    end
+  end
+
 end
