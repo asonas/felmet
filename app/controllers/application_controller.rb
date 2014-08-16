b5025845af8ae4967ce773af86c7d4fd3b8c8f43
asonas <hzw1258@gmail.com>
2014-08-17 05:07:41 +0900

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :auth
+
+  def auth
+    secrets = Rails.application.secrets
+    return unless secrets.basic_auth_user && secrets.basic_auth_pass
+    unless Rails.env.development?
+      authenticate_or_request_with_http_basic do |user, pass|
+        user == secrets.basic_auth_user && pass == secrets.basic_auth_pass
+      end
+    end
+  end
+
 end
