398cdceba870b05d052eba06d6c8008cc06a5c98
asonas <hzw1258@gmail.com>
2015-05-06 21:27:27 +0900

    Add sum_enrollment_time

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,14 +8,36 @@ class Event < ActiveRecord::Base
     UserMailer.checkin(self).deliver
   end
 
+  after_initialize :calculate_enrollment_time
+
+  attr_accessor :hours, :minutes
+
   scope :fetch_at, ->(date) { where(checkin_at: date.at_beginning_of_month..date.at_end_of_month) }
+
+  # TODO EventService? EventEntity?
+  def self.sum_enrollment_time(events)
+    hours = []
+    minutes = []
+    events.each do |event|
+      next if event.open?
+      hours.push event.hours[0]
+      minutes.push event.minutes[0]
+    end
+
+    "#{hours.sum}時間 #{minutes.sum}分"
+  end
+
+  def calculate_enrollment_time
+    return nil if new_record?
+    return nil if open?
+    days = (checkout_at - checkin_at).divmod(24*60*60)
+    @hours = days[1].divmod(60*60)
+    @minutes = hours[1].divmod(60)
+  end
 
   def enrollment_time
     return "在籍中です" if open?
-    days = (checkout_at - checkin_at).divmod(24*60*60)
-    hours = days[1].divmod(60*60)
-    minutes = hours[1].divmod(60)
-    "#{hours[0].to_i}時間 #{minutes[0].to_i}分"
+    "#{@hours[0].to_i}時間 #{@minutes[0].to_i}分"
   end
 
   def checkout!
