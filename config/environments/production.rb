56d16aeb6434663d2e093903c78d2330b3ef4edb
asonas <hzw1258@gmail.com>
2017-03-28 21:22:33 +0900

    Change mandrill to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'felmet.herokuapp.com' }
   config.action_mailer.smtp_settings = {
-    address:              "smtp.mandrillapp.com",
-    port:                 587, # ports 587 and 2525 are also supported with STARTTLS
-    enable_starttls_auto: true, # detects and uses STARTTLS
-    user_name:            ENV['MANDRILL_USERNAME'],
-    password:             ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
-    authentication:       'login', # Mandrill supports 'plain' or 'login'
-    #domain:               'yourdomain.com', # your domain to identify your server when connecting
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
